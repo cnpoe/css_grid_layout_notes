b73b4d431452c573e8ab12ee9401e5a4a0411873
cnpoe <cnpoek@gmail.com>
2019-07-21 04:22:44 -0500

    hide bar with transition

--- grid_layout/script.js
+++ grid_layout/script.js
@@ -1,15 +1,27 @@
 function openNavBar() {
   console.log('open')
-  document.getElementsByTagName('body')[0].style.
-    gridTemplateColumns = "290px [col-main] 2fr 1fr";
+  let content = document.querySelector('body')
+  if (content.classList.contains('animate')) {
+    content.classList.remove('animate')
+  } else {
+    content.classList.add('animate')
+  }
   document.getElementById('burger').style.display = "none"
   document.getElementById('no-burger').style.display = "inherit"
+  // document.getElementsByTagName('body')[0].style.
+  //   gridTemplateColumns = "290px [col-main] 2fr 1fr";
 }
 
 function collapseNavBar() {
   console.log('close')
-  document.getElementsByTagName('body')[0].style.
-    gridTemplateColumns = "70px [col-main] 2fr 1fr";
-    document.getElementById('burger').style.display = "inherit"
-    document.getElementById('no-burger').style.display = "none"
+  let content = document.querySelector('body')
+  if (content.classList.contains('animate')) {
+    content.classList.remove('animate')
+  } else {
+    content.classList.add('animate')
+  }
+  document.getElementById('burger').style.display = "inherit"
+  document.getElementById('no-burger').style.display = "none"
+  // document.getElementsByTagName('body')[0].style.
+  //   gridTemplateColumns = "70px [col-main] 2fr 1fr";
 }
